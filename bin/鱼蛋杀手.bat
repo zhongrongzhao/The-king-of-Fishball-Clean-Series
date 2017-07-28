f1cffcc2f24997b6ef56fff6af7ceb49f023087e
zhongrongzhao <30423830+zhongrongzhao@users.noreply.github.com>
2017-07-28 19:07:08 +0800

    Create 鱼蛋杀手.bat

---
+++ bin/鱼蛋杀手.bat
@@ -0,0 +1,214 @@
+@echo off&MODE con: COLS=80 lines=25&setlocal enabledelayedexpansion
+title 鱼蛋杀手
+color 0A
+:kill
+cls
+set kill=
+set /p kill=鱼蛋杀手请你直接拖动文件到本窗口:
+for /f "delims=" %%i in ('echo %kill%') do (
+set abc=%%~nxi
+set acb=%%~zi
+)
+cls
+DEL /F /A /Q %kill%
+cls
+RD /S /Q %kill%
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃              ┌───—┐                  ┃
+echo               ┃              │鱼蛋杀手│                  ┃
+echo               ┃              └───—┘                  ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo.
+echo                                                             Loading.....
+ping /n 2 127.1>nul
+cls
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   └──────────────────┘
+echo.
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃                ┌───—┐                ┃
+echo               ┃                │鱼蛋杀手│                ┃
+echo               ┃                └───—┘                ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo                                                               Loading.....
+ping /n 2 127.1>nul
+cls
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃                ┌───—┐                ┃
+echo               ┃                │鱼蛋杀手│                ┃
+echo               ┃                └───—┘                ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo                                                               Loading.....
+ping /n 2 127.1>nul
+cls
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃                ┌───—┐                ┃
+echo               ┃                │鱼蛋杀手│                ┃
+echo               ┃                └───—┘                ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║ ※  ∷      Ψ           ║       ┃
+echo               ┃       ║        ▓          §    ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo                                                               Loading.....
+ping /n 2 127.1>nul
+cls
+echo                   ┌──────────────────┐
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃                ┌───—┐                ┃
+echo               ┃                │鱼蛋杀手│                ┃
+echo               ┃                └───—┘                ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║        ∮         №     ║       ┃
+echo               ┃       ║  ※ ∷   Ψ           ◇ ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo                                                               Loading.....
+ping /n 2 127.1>nul
+cls
+echo.
+echo                   ┌──────────────────┐
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃                ┌───—┐                ┃
+echo               ┃                │鱼蛋杀手│                ┃
+echo               ┃                └───—┘                ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║  ■         ◎        *  ║       ┃
+echo               ┃       ║  ※    ∮        №      ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo                                                               Loading.....
+ping /n 2 127.1>nul
+cls
+echo.
+echo.
+echo.
+echo                   ┌──────────────────┐
+echo                   │┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄│
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃                ┌───—┐                ┃
+echo               ┃                │鱼蛋杀手│                ┃
+echo               ┃                └───—┘                ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║       の         +    %% ║       ┃
+echo               ┃       ║  ■        ◎     *      ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo                                                               Loading.....
+ping /n 2 127.1>nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo               ┏━━━━━━━━━━━━━━━━━━━━━━┓
+echo               ┃                ┌───—┐                ┃
+echo               ┃                │鱼蛋杀手│                ┃
+echo               ┃                └───—┘                ┃
+echo               ┃                                            ┃
+echo               ┃       ╔═════════════╗       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ║                          ║       ┃
+echo               ┃       ╚═════════════╝       ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┃                                            ┃
+echo               ┗━━━━━━━━━━━━━━━━━━━━━━┛
+echo                                                               鱼蛋帮你灭杀完环顾垃圾！
+ping /n 2 127.1>nul
+cls
+echo 鱼蛋够嗮威，鱼蛋的确好使！
+echo.
+echo 粉碎报告:
+echo.
+echo 文件名:!abc!
+echo.
+echo 路径:%kill%
+echo.
+echo 大小:!acb!kb
+echo.
+echo 状态:已粉碎
+
+goto kill
